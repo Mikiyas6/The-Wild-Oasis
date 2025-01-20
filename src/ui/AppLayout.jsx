3c83cbb84a8eca8feb3f8297d18aa1e45f146e49
Mikiyas6 <Mikitedi9@gmail.com>
2025-01-20 13:01:07 +0300

    Building the App Layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  min-height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: green;
+  padding: 4rem 4.8rem 6.4rem;
+  background-color: var(--color-grey-50);
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Sidebar />
+      <Header />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;
