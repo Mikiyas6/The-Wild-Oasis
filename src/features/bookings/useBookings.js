1ab3473f96c5ac739124cb83538a58fc6081cd1e
Mikiyas6 <Mikitedi9@gmail.com>
2025-03-21 12:46:52 +0300

    Prefetching With React Query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   /*
 useQuery is a React hook used inside components that acts as a bridge between our components and the QueryClient, offering:
 ✅ The actual data fetching using queryFn.
@@ -30,6 +32,8 @@ How It Works
   const sortBy = { field, direction };
   // Pagination
   const page = Number(searchParams.get("page")) || 1;
+
+  //Query
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -70,6 +74,22 @@ The `useQuery` hook returns several values, but the most important ones are:
 - If you want to fetch fresh data on demand, call `refetch()`.  
 
 */
+
+  //Pre-Fetching
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
 
   return { isLoading, bookings, error, count };
 }
