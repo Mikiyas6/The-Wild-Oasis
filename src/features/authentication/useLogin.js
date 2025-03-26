298bb822cffa45ccf922e4c690329e5cd6d6003d
Mikiyas6 <Mikitedi9@gmail.com>
2025-03-26 09:07:57 +0300

    fixing small bug related to Authentication

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       toast.success("Login successful");
-      queryClient.setQueryData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/", { replace: true });
     },
     onError: (err) => {
